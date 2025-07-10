ed2d3882bfe8a459786fcc08caef29b56d556384
DerekDao01 <dddao1@uci.edu>
2025-07-09 21:48:35 -0700

    Initial commit

---
+++ src/components/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+    // Javascript XML
+    const Name = "dook"
+    if (Name){
+        return <h1>Hello {Name}</h1>
+    }
+    return <h1>Hello World</h1>
+}
+
+export default Message;
